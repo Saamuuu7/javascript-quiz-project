dbaa83348f7d1d5fa189802d514c87d7867b076b
Samuel Pérez <samuelperez82pm@gmail.com>
2024-04-15 16:25:39 +0200

    part1 done

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,50 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions //array
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
     // 2. getQuestion()
-    
+    getQuestion() {
+
+        return this.questions[this.currentQuestionIndex]
+
+    }
+
     // 3. moveToNextQuestion()
 
+    moveToNextQuestion() {
+        this.currentQuestionIndex++
+    }
+
+
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        this.questions.sort((a, b) => 0.5 - Math.random())
+    }
+
 
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        if (answer) {
+            this.correctAnswers++
+        }
+    }
+
+
 
     // 6. hasEnded()
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        } else if (this.currentQuestionIndex === this.questions.length) {
+            return true
+        }
+    }
 }
